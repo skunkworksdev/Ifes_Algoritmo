f3901523565ff61f062440fe830f93fd468781a8
skunkworksdev <76817702+skunkworksdev@users.noreply.github.com>
2021-03-15 12:05:43 -0300

    question1 - resolucao

--- codigos/question1.py
+++ codigos/question1.py
@@ -4,25 +4,43 @@ Created on Wed Feb 24 14:27:34 2021
 @author: luizm
 """
 
-idade_pessoas = {
-    'Ana': 7,
-    'José': 38,
-    'Paulo': 22,
-    'Lucia': 59,
-    'Cristina': 44,
-    'Luiz': 12
-}
+# idade_pessoas = {
+#     'Ana': 7,
+#     'José': 38,
+#     'Paulo': 22,
+#     'Lucia': 59,
+#     'Cristina': 44,
+#     'Luiz': 12
+# }
+#
+# maiores_de_idade = []
+# menores_de_idade = []
+#
+# while len(idade_pessoas) >= 0:
+#     pessoas = list(idade_pessoas.keys())
+#     pessoa = pessoas[0]
+#
+#     if idade_pessoas[pessoa] >= 18:
+#         maiores_de_idade.append(pessoa)
+#     else:
+#         menores_de_idade.append(pessoa)
+#
+#     del idade_pessoas[pessoa]
 
-maiores_de_idade = []
-menores_de_idade = []
+# Fibbonacci
 
-while len(idade_pessoas) >= 0:
-    pessoas = list(idade_pessoas.keys())
-    pessoa = pessoas[0]
+n = int(input("Que termo deseja encontrar: "))
+ultimo=1
+penultimo=1
 
-    if idade_pessoas[pessoa] >= 18:
-        maiores_de_idade.append(pessoa)
-    else:
-        menores_de_idade.append(pessoa)
 
-    del idade_pessoas[pessoa]
+if (n==1) or (n==2):
+    print("1")
+else:
+    count=3
+    while count <= n:
+        termo = (ultimo + penultimo) * 2
+        penultimo = ultimo
+        ultimo = termo
+        count += 1
+    print(termo)
